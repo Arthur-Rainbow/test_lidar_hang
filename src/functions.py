52d56e7e6529b1d592b34a51cd739f7af7f2df6f
Ian <librylibry@gmail.com>
2026-03-12 00:02:17 +0100

    CityJSON plus propre

--- src/functions.py
+++ src/functions.py
@@ -9,3 +9,24 @@ def estimer_z_toit(xy, tree, Z, k=20):
 
     z = Z[np.array(indices)]
     return float(np.median(z))
+
+
+def fit_plane_svd(x, y, z):
+    pts = np.column_stack([x, y, z])
+    centroid = pts.mean(axis=0)
+    centered = pts - centroid
+    _, _, vh = np.linalg.svd(centered)
+    normal = vh[-1]
+    d = -np.dot(normal, centroid)
+    return normal, d, centroid
+
+
+def plane_z(x, y, normal, d):
+    a, b, c = normal
+    return -(a * x + b * y + d) / c
+
+def triangle_max_edge_length(coords_2d):
+    a = np.linalg.norm(coords_2d[0] - coords_2d[1])
+    b = np.linalg.norm(coords_2d[1] - coords_2d[2])
+    c = np.linalg.norm(coords_2d[2] - coords_2d[0])
+    return max(a, b, c)
